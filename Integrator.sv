6d04b8dd7a2469861469b26782605b13634d7992
susienguyen1810 <113402380+susienguyen1810@users.noreply.github.com>
2022-10-25 12:35:08 -0700

    Final project 2

--- Integrator.sv
+++ Integrator.sv
@@ -2,8 +2,12 @@ module Integrator (input logic Start, Clk, Reset,
 						output logic [15:0] SUM, 
 						output logic Done);
 						
-logic [2:0]state;
-logic [2:0]nextstate;
+logic [3:0]state;
+logic [3:0]nextstate;
+logic [15:0] sum;
+
+assign SUM = ( Done == 1) ? sum : 16'hxx;
+
 
 // insert your code here, 
 // do not change any ports specifications. You are not required to write a testbench for this design. 
@@ -28,59 +32,65 @@ end
 always_comb begin
 		 case(state) 
 		 4'b0000 : begin //s0
-		 SUM = 0;
+		 sum = 0;
 		 Done = 0;
 		 if (Start ==1'b1) nextstate = 4'b0001;
 		 else	nextstate = 4'b0000;
 		 end
 		 
 		 4'b0001 : begin //s1
-		 SUM = ROM[0];
+		 sum = ROM[0];
 		 Done = 0;
 		 nextstate = 4'b0010;
 		 end
 	
 		 4'b0010 : begin //s2
-		 SUM = ROM[0] + ROM[1];
+		 sum = ROM[0] + ROM[1];
 		 Done = 0;
 		 nextstate = 4'b0011;
 		 end
 		 
 		 4'b0011 : begin //s3
-		 SUM = ROM[0] + ROM[1] + ROM[2];
+		 sum = ROM[0] + ROM[1] + ROM[2];
 		 Done = 0;
 		 nextstate = 4'b0100;
 		 end
 		 
 		 4'b0100 : begin //s4
-		 SUM = ROM[0] + ROM[1] + ROM[2] + ROM[3];
+		 sum = ROM[0] + ROM[1] + ROM[2] + ROM[3];
 		 Done = 0;
 		 nextstate = 4'b0101;
 		 end
 	
 		 4'b0101 : begin //s5
-		 SUM = ROM[0] + ROM[1] + ROM[2] + ROM[3] + ROM[4];
+		 sum = ROM[0] + ROM[1] + ROM[2] + ROM[3] + ROM[4];
 		 Done = 0;
 		 nextstate = 4'b0110;
 		 end
 	
 		 4'b0110 : begin //s6
-		 SUM = ROM[0] + ROM[1] + ROM[2] + ROM[3] + ROM[4] + ROM[5];
+		 sum = ROM[0] + ROM[1] + ROM[2] + ROM[3] + ROM[4] + ROM[5];
 		 Done = 0;
 		 nextstate = 4'b0111;
 		 end
 		 
 		 4'b0111 : begin //s7
-		 SUM = ROM[0] + ROM[1] + ROM[2] + ROM[3] + ROM[4] + ROM[5] + ROM[6];
+		 sum = ROM[0] + ROM[1] + ROM[2] + ROM[3] + ROM[4] + ROM[5] + ROM[6];
 		 Done = 0;
 		 nextstate = 4'b1000;
 		 end
 		 
 		 4'b1000 : begin //s8
-		 SUM = ROM[0] + ROM[1] + ROM[2] + ROM[3] + ROM[4] + ROM[5] + ROM[6] + ROM[7];
-		 Done = 1'b1;
+		 sum = ROM[0] + ROM[1] + ROM[2] + ROM[3] + ROM[4] + ROM[5] + ROM[6] + ROM[7];
+		 Done = 1;
 		 nextstate = 4'b0000;
 		 end
+		 default: begin
+		 nextstate=4'b0000;
+		 sum = 0;
+		 Done = 0;
+		 end
+		 
 		 endcase
 		 end
 
