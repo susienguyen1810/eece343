bfae78b2657acef89d68fbe3a5befb73590f7279
susienguyen1810 <113402380+susienguyen1810@users.noreply.github.com>
2022-11-15 09:15:17 -0800

    activity 10

--- activity9.sv
+++ activity9.sv
@@ -2,26 +2,44 @@ module activity9 #(
 			 parameter DATA_WIDTH = 32)
 			 (output logic [DATA_WIDTH-1:0] instructionoutput, 
 			 input logic [DATA_WIDTH-1:0] BranchAdd,
-			 output logic [DATA_WIDTH-1:0] PCPLUS4,
+			 //output logic [DATA_WIDTH-1:0] PCPLUS4,
 			 input clk, reset,
-			 input logic PCSrc);
+			 input logic PCSrc,
 			 
+			 input logic [10:0] Instr,
+			 input logic [31:0] PCPlus4,
+		    input logic [1:0] RegSrc, RegWrite, ImmScr, ALUScr,ImmSrc,
+			 input logic WE3,
+			 output logic [31:0] SrcA,
+			 output logic [31:0] SrcB,
+			 output logic [31:0] WriteData,
+			 output logic [31:0] RD2
+			 );
 			 
-			 
+			 logic [31:0] RD,Extlmm,WD3;
+			 logic [3:0] RA1,RA2,WA3;
 			 logic [DATA_WIDTH-1:0] wPC,wA;
+			 logic [31:0] PCPLUS4, PCPLUS8,R15;
+			 logic [31:0] Result;
 			 
 			 //implement modules
 			 ff    pc(.clk(clk),.PC(wPC), .A(wA), .reset(reset));
-			 adder addy(.A(wA), .PCPlus4(PCPLUS4));
-	       PCSrc plexi(.PCPlus4(PCPLUS4), .BranchAdd(BranchAdd), .PCSrc(PCSrc), .PC(wPC));
+			 adder PC4Adder(.A(wA), .B(32'd4), .PCPlus4(PCPLUS4));
+			 adder PC8Adder(.A(PCPLUS4), .B(32'd4), .PCPlus4(PCPLUS8));
+	       muxx plexi(.PCPlus4(PCPLUS4), .BranchAdd(BranchAdd), .PCSrc(PCSrc), .PC(wPC));
 			 Instruction ROM(.RD(instructionoutput),.A(wA));
-
+			 muxx #(4)  mux1(.PCPlus4(RD[19:16]), .BranchAdd(4'b1111), .PCSrc(RegSrc[0]), .PC(RA1));	
+			 muxx #(4)  mux2(.PCPlus4(RD[3:0]), .BranchAdd(RD[15:12]), .PCSrc(RegSrc[1]), .PC(RA2));
+			 muxx ALUmux(.PCPlus4(RD2), .BranchAdd(Extlmm), .PCSrc(ALUSrc), .PC(SrcB));
+			 registerfile Reg(.clk(clk), .WE3(WE3), .RA1(RA1), .RA2(RA2), .WA3(RD[15:12]), .WD3(Result), .R15(PCPLUS8), .RD1(SrcA), .RD2(RD2));
+			 extend myextend(.Instr(RD[23:0]), .ImmSrc(ImmSrc), .Extlmm(Extlmm));
 endmodule 
 //Multiplexer determine what address to use for PC
-module PCSrc (input logic [31:0]PCPlus4,
-				  input logic [31:0]BranchAdd,
+module muxx #(parameter DATA_WIDTH = 32)
+				 (input logic [DATA_WIDTH-1:0]PCPlus4,
+				  input logic [DATA_WIDTH-1:0]BranchAdd,
 				  input logic  PCSrc, 
-				  output logic[31:0]PC);
+				  output logic[DATA_WIDTH-1:0]PC);
 	//if load is 1 branch address
 	//if load is 0 PCPlus4
 	always_comb begin	
@@ -45,10 +63,11 @@ module ff   (input logic clk, reset,
 endmodule
 
 //Adder
-module adder (input logic [31:0] A,
-				  output logic[31:0] PCPlus4);
+module adder #(parameter DATA_WIDTH = 32)
+				 (input logic [DATA_WIDTH-1:0] A, B,
+				  output logic[DATA_WIDTH-1:0] PCPlus4);
 	// takes padded value and sum, adds them to data
-	assign PCPlus4 = A + 4'b0100;
+	assign PCPlus4 = A + B;
 	
 //	//passes data to the multiplexer
 //	PCPLUS4 <= PCPlus4;
